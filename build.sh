0601542ef945e2bb7d7248fa4d012a63e1776088
bigjeff96 <joepep271996@gmail.com>
2023-03-30 09:34:42 +0200

    better build script

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ DEBUG="-o:minimal -debug -use-separate-modules -thread-count:8"
 FAST="-o:speed -no-bounds-check"
 
 set -e
-$ODIN run . $DEBUG -define:DEFAULT_TEMP_ALLOCATOR_BACKING_SIZE=20_000_000
-# ./plot.py 
+echo BUILD:
+time $ODIN build . $DEBUG -define:DEFAULT_TEMP_ALLOCATOR_BACKING_SIZE=20_000_000
+echo OUTPUT:
+./master_thesis.bin
