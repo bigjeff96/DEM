122d618df3c2cabd541e70f1816f53c3884dac03
bigjeff96 <joepep271996@gmail.com>
2023-04-08 03:34:15 +0200

    updated odin compiler -> changed the ols.json

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 ODIN=odin
 
-DEBUG="-o:minimal -debug -use-separate-modules -thread-count:8"
+DEBUG="-o:none -debug -use-separate-modules -thread-count:8"
 FAST="-o:speed -no-bounds-check"
 
 set -e
 echo BUILD:
-time $ODIN build . $DEBUG -define:DEFAULT_TEMP_ALLOCATOR_BACKING_SIZE=20_000_000
+time $ODIN build . $FAST -define:DEFAULT_TEMP_ALLOCATOR_BACKING_SIZE=20_000_000
 echo OUTPUT:
 ./master_thesis.bin
